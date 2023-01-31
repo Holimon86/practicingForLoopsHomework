1697fec83aeebdf450c49c2611315c479237b16d
Douglas <mercurymuur86@yahoo.com>
2023-01-31 00:27:44 -0500

    forLoops

---
+++ index.js
@@ -0,0 +1,34 @@
+let myPhone = [];
+
+for (let i = 0; i <= 10; i++) {
+    myPhone.push(i);
+    console.log(myPhone)
+} 
+
+let myHouse = [];
+
+let i = 0;
+ while(i <= 10) {
+    myHouse.push(i);
+    i++;
+    console.log(myHouse);
+ }
+
+ let myFamily = [];
+
+ for (let i = 11; i <= 21; i+= 2) {
+     myFamily.push(i);
+     console.log(myFamily)
+ } 
+ 
+ let myCar = [];
+
+ for (let i = 15; i > 0; i-= 2) {
+     myCar.push(i);
+     console.log(myCar)
+ } 
+ 
+
+
+
+
